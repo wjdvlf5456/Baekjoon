f98a3cb6619306067f748c8c44478f8afc460fef
최정필 <wjdvlf1407@gmail.com>
2022-04-23 20:41:03 +0900

    입출력과 사칙연산 6번

---
+++ src/com/baekjoon/ex01/Q1001.java
@@ -0,0 +1,21 @@
+package com.baekjoon.ex01;
+
+import java.util.Scanner;
+
+public class Q1001 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int numA = sc.nextInt();
+		int numB = sc.nextInt();
+		
+		System.out.println(numA-numB);
+		
+		sc.close();
+		
+		
+	}
+
+}
