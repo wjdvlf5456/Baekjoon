e86e04efc1e3e2ff461538eaa97921abf839460c
최정필 <wjdvlf1407@gmail.com>
2022-04-23 21:33:02 +0900

    조건문 5번

---
+++ src/com/baekjoon/ex02/Q2884.java
@@ -0,0 +1,37 @@
+package com.baekjoon.ex02;
+
+import java.util.Scanner;
+
+public class Q2884 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int hour = sc.nextInt();
+		int minute = sc.nextInt();
+		
+		
+		if (hour>0 && hour<24) {
+			if (minute>=45) {
+				System.out.println(hour+" "+(minute-45));
+			} else {
+				System.out.println((hour-1)+" "+(minute+15));
+			}
+			
+		} else if(hour==0){
+			if (minute>=45) {
+				System.out.println(hour+" "+(minute-45));
+			} else {
+				System.out.println((hour+23)+" "+(minute+15));
+
+			}
+
+		}
+		
+		sc.close();
+		
+		
+	}
+
+}
