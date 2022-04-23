d806fca16ac48a92dbd1156540409e2d392bfa71
최정필 <wjdvlf1407@gmail.com>
2022-04-23 20:36:38 +0900

    입출력과 사칙연산 5번

---
+++ src/com/baekjoon/ex01/Q1000.java
@@ -0,0 +1,14 @@
+package com.baekjoon.ex01;
+
+public class Q1000 {
+	
+	public static void main(String[] args) {
+		
+		int num1 = 1;
+		int num2 = 2;
+		
+		System.out.println(num1+num2);
+		
+	}
+
+}
