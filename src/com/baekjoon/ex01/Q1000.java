907a04764ffbde7de4895aa6ebb780904a348e31
최정필 <wjdvlf1407@gmail.com>
2022-04-23 20:38:58 +0900

    입출력과 사칙연산 5번 2

--- src/com/baekjoon/ex01/Q1000.java
+++ src/com/baekjoon/ex01/Q1000.java
@@ -1,13 +1,19 @@
 package com.baekjoon.ex01;
 
+import java.util.Scanner;
+
 public class Q1000 {
 	
 	public static void main(String[] args) {
 		
-		int num1 = 1;
-		int num2 = 2;
+		Scanner sc = new Scanner(System.in);
+		
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
 		
 		System.out.println(num1+num2);
+		
+		sc.close();
 		
 	}
 
