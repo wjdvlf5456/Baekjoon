22761aa74c94e119e44ae313892e00b3d4b64c61
최정필 <wjdvlf1407@gmail.com>
2022-04-23 20:27:31 +0900

    입출력과 사칙연산 3번

---
+++ src/com/baekjoon/ex01/Q10171.java
@@ -0,0 +1,14 @@
+package com.baekjoon.ex01;
+
+public class Q10171 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+		
+	}
+
+}
