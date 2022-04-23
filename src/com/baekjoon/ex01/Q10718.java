518304c1b26a809824ef1e20cd6e66a0df772c79
최정필 <wjdvlf1407@gmail.com>
2022-04-23 20:21:54 +0900

    입출력과 사칙연산 2 정답

--- src/com/baekjoon/ex01/Q10718.java
+++ src/com/baekjoon/ex01/Q10718.java
@@ -3,8 +3,8 @@ package com.baekjoon.ex01;
 public class Q10718 {
 
 	public static void main(String[] args) {
-		System.out.println("\"강한친구 대한육군\"");
-		System.out.println("\"강한친구 대한육군\"");
+		System.out.println("강한친구 대한육군");
+		System.out.println("강한친구 대한육군");
 
 	}
 
