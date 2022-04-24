f28466743ea6f043c687456fd9be53d10094b7dc
최정필 <wjdvlf1407@gmail.com>
2022-04-24 14:43:47 +0900

    반복문 5번

---
+++ src/com/baekjoon/ex03/Q2741.java
@@ -0,0 +1,21 @@
+package com.baekjoon.ex03;
+
+import java.util.Scanner;
+
+public class Q2741 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt();
+		
+		for (int i = 1; i <= num ; i++) {
+			System.out.println(i);
+		}
+		
+		sc.close();
+		
+	}
+
+}
