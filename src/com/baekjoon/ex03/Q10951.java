6a61627f770a4aa104bf6279bdf6be1de25f696e
최정필 <wjdvlf1407@gmail.com>
2022-04-24 17:00:56 +0900

    반복문 13번

---
+++ src/com/baekjoon/ex03/Q10951.java
@@ -0,0 +1,22 @@
+package com.baekjoon.ex03;
+
+import java.util.Scanner;
+
+public class Q10951 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		while (sc.hasNext()) {
+			int numA = sc.nextInt();
+			int numB = sc.nextInt();
+
+			System.out.println(numA + numB);
+		}
+
+		sc.close();
+
+	}
+
+}
