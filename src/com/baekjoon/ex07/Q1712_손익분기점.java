00a923f5ce2156b22ff3f1eb9d5f659aca948d5a
최정필 <wjdvlf1407@gmail.com>
2022-09-10 16:37:37 +0900

    손익분기점 System.out

---
+++ src/com/baekjoon/ex07/Q1712_손익분기점.java
@@ -0,0 +1,32 @@
+package com.baekjoon.ex07;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q1712_손익분기점 {
+	
+	public static void main(String[] args)throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int a = Integer.parseInt(st.nextToken());	// 고정 비용
+		int b = Integer.parseInt(st.nextToken());	// 가변 비용
+		int c = Integer.parseInt(st.nextToken());	// 제품 가격
+		
+		// 판매량
+		int salesRate = 0;
+		
+		if (b>=c) {
+			salesRate = -1;
+		} else {
+			salesRate = a/(c-b)+1;
+		}
+		System.out.println(salesRate);
+		
+		br.close();
+	}
+
+}
