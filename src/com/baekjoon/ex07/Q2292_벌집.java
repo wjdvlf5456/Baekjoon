4636bba02626d073c136b1cdb82d33dbd461f112
최정필 <wjdvlf1407@gmail.com>
2022-09-10 17:04:19 +0900

    벌집 정답

---
+++ src/com/baekjoon/ex07/Q2292_벌집.java
@@ -0,0 +1,30 @@
+package com.baekjoon.ex07;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q2292_벌집 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(br.readLine());
+
+		int i = 6;
+		int count = 1;
+
+		while (n > 1) {
+			n = n - i;
+			count++;
+			i += 6;
+
+		}
+
+		System.out.println(count);
+
+		br.close();
+
+	}
+
+}
