9a143f35096b5c3a8fd2194a82c0452052af3880
최정필 <wjdvlf1407@gmail.com>
2022-09-11 21:58:12 +0900

    달팽이 정답

--- src/com/baekjoon/ex07/Q4_2869_달팽이는올라가고싶다.java
+++ src/com/baekjoon/ex07/Q4_2869_달팽이는올라가고싶다.java
@@ -9,6 +9,13 @@ public class Q4_2869_달팽이는올라가고싶다 {
 
 	public static void main(String[] args) throws IOException {
 
+		/* 반례모음
+		 9 1 100			13
+		 2 1 5 				4
+		 100 99 1000000000	999999901			999999901 == 1000000000-99
+		
+		*/
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
@@ -16,21 +23,22 @@ public class Q4_2869_달팽이는올라가고싶다 {
 		int b = Integer.parseInt(st.nextToken());
 		int x = Integer.parseInt(st.nextToken());
 
-		int current = 0;
-		int count = 0;
+		// 하루동안 이동하는 거리
+		int perDay = a - b;
+		int night = x - b;
 
-		while (current < x) {
-			current += a;
-			count++;
-			if (current >= x) {
-				break;
-			}
+		int result = 0;
 
-			current -= b;
+		// 나머지가 있으면 하루 추가
+		if (night % perDay > 0) {
+			result = night / perDay + 1;
+
+		} else {
+			result = night / perDay;
 
 		}
 		
-		System.out.println(count);
+		System.out.println(result);
 
 		br.close();
 
