75a01ea6e3b23fae3cdddbca2de2eb9c644ae5d0
최정필 <wjdvlf1407@gmail.com>
2022-09-16 20:32:56 +0900

    주석 추가

--- src/com/baekjoon/ex08/Q1_1978_소수찾기.java
+++ src/com/baekjoon/ex08/Q1_1978_소수찾기.java
@@ -14,6 +14,7 @@ public class Q1_1978_소수찾기 {
 
 		int n = Integer.parseInt(st.nextToken());
 
+		// 입력받은 수를 배열로 저장
 		int[] data = new int[n];
 
 		st = new StringTokenizer(br.readLine());
@@ -23,12 +24,14 @@ public class Q1_1978_소수찾기 {
 
 		}
 
+		// 오름차순 정렬
 		Arrays.sort(data);
 
 		// 소수의 개수
 		int demical = 0;
 
 		for (int i = 0; i < data.length; i++) {
+			// 카운트가 한 번이라도 되면 소수가 아니다.
 			int count = 0;
 			if (data[i] >= 2) {
 				for (int j = 2; j < data[i]; j++) {
