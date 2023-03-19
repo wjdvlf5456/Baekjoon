9733509051e92bac7c47817d9719a4b23395abaf
최정필 <wjdvlf1407@gmail.com>
2023-03-19 14:20:48 +0900

    피보나치수2 int 자료형이라 틀림

---
+++ src/com/baekjoon/study/Ex2748_피보나치수2.java
@@ -0,0 +1,34 @@
+package com.baekjoon.study;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Ex2748_피보나치수2 {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int num = Integer.parseInt(st.nextToken());
+
+		int[] numArray = new int[num + 1];
+
+		numArray[0] = 0;
+		numArray[1] = 1;
+		numArray[2] = 1;
+
+		for (int i = 3; i < numArray.length; i++) {
+			numArray[i] = numArray[i - 2] + numArray[i - 1];
+		}
+
+		System.out.println(numArray[num]);
+
+		br.close();
+
+	}
+
+}
