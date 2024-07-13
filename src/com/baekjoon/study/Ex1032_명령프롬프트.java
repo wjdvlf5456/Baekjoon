2d1b9564becb2731ee19f941f410152c78015bdb
최정필 <wjdvlf1407@gmail.com>
2024-07-13 18:59:07 +0900

    3

---
+++ src/com/baekjoon/study/Ex1032_명령프롬프트.java
@@ -0,0 +1,46 @@
+package com.baekjoon.study;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Ex1032_명령프롬프트 {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int n = Integer.parseInt(st.nextToken());
+
+		String[] result;
+		result = br.readLine().split("(?<!^)");
+
+		String[] daum;
+
+		for (int i = 1; i < n; i++) {
+			daum = br.readLine().split("(?<!^)");
+
+			for (int j = 0; j < result.length; j++) {
+				if (result[j].equals(daum[j])) {
+
+				} else {
+					result[j] = "?";
+				}
+
+			}
+
+		}
+
+		for (int i = 0; i < result.length; i++) {
+			System.out.print(result[i]);
+		}
+
+		br.close();
+
+	}
+
+}
