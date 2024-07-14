be3c26b1d296d156e3f38e86e2a5ea12529c2653
최정필 <wjdvlf1407@gmail.com>
2024-07-14 18:27:34 +0900

    33

---
+++ src/com/baekjoon/study/Ex1009_분산처리.java
@@ -0,0 +1,50 @@
+package com.baekjoon.study;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Ex1009_분산처리 {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int t = Integer.parseInt(br.readLine());
+
+		int a = 0;
+		int b = 0;
+
+		int result = 0;
+
+		StringTokenizer st;
+		for (int i = 0; i < t; i++) {
+			st = new StringTokenizer(br.readLine());
+			a = Integer.parseInt(st.nextToken());
+			b = Integer.parseInt(st.nextToken());
+
+			result = a;
+
+			for (int j = 0; j < b - 1; j++) {
+				result = (result * a % 10);
+
+			}
+
+			if (result > 9) {
+				result = result % 10;
+			}
+			if (result == 0) {
+				result = 10;
+			}
+
+			System.out.println(result);
+			result = 0;
+
+		}
+
+		br.close();
+
+	}
+
+}
