40f1258ddd09d3bb1adc336b3c1fa172a1b6c088
최정필 <wjdvlf1407@gmail.com>
2024-07-19 22:32:02 +0900

    11

---
+++ src/com/baekjoon/study/Ex5597_과제안내신분.java
@@ -0,0 +1,33 @@
+package com.baekjoon.study;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Ex5597_과제안내신분 {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		Boolean[] student = new Boolean[30];
+
+		for (int i = 0; i < student.length; i++) {
+			student[i] = false;
+		}
+
+		for (int i = 0; i < 28; i++) {
+			student[Integer.parseInt(br.readLine()) - 1] = true;
+		}
+
+		for (int i = 0; i < 30; i++) {
+			if (student[i] == false) {
+				System.out.println(i + 1);
+			}
+		}
+
+		br.close();
+
+	}
+
+}
