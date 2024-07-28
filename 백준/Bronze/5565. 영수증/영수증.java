5a788709d38427736e27b13362f13cdb24c4b965
최정필 <103472023+wjdvlf5456@users.noreply.github.com>
2024-07-28 17:40:40 +0900

    [Bronze III] Title: 영수증, Time: 104 ms, Memory: 12824 KB -BaekjoonHub

---
+++ 백준/Bronze/5565. 영수증/영수증.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int all =  sc.nextInt();
+		
+		for (int i = 0; i < 9; i++) {
+			all = all - sc.nextInt();
+		}
+		System.out.println(all);
+		
+
+		sc.close();
+
+	}
+
+}
