c0bdb97e741a55ee367cfe729241adc481c010e6
최정필 <103472023+wjdvlf5456@users.noreply.github.com>
2024-07-28 17:43:28 +0900

    [Bronze III] Title: 영수증, Time: 80 ms, Memory: 11360 KB -BaekjoonHub

--- 백준/Bronze/5565. 영수증/영수증.java
+++ 백준/Bronze/5565. 영수증/영수증.java
@@ -1,19 +1,28 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
-		Scanner sc = new Scanner(System.in);
-		
-		int all =  sc.nextInt();
-		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int all = Integer.parseInt(br.readLine());
+
 		for (int i = 0; i < 9; i++) {
-			all = all - sc.nextInt();
-		}
-		System.out.println(all);
-		
+			all = all - Integer.parseInt(br.readLine());
 
-		sc.close();
+		}
+
+		bw.append(Integer.toString(all));
+
+		bw.flush();
+		bw.close();
+		br.close();
+
 
 	}
 
