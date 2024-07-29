9b4937a7ea774922476231457147929e15a05f0e
최정필 <wjdvlf1407@gmail.com>
2024-07-29 16:59:18 +0900

    그릇

---
+++ src/com/baekjoon/study/Ex7567_그릇.java
@@ -0,0 +1,36 @@
+package com.baekjoon.study;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Ex7567_그릇 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		String[] plates = br.readLine().split("(?<!^)");
+
+		int height = 10;
+
+		for (int i = 1; i < plates.length; i++) {
+			if (plates[i].equals(plates[i-1])) {
+				height += 5;
+			} else {
+				height += 10;
+			}
+
+		}
+
+		String result = Integer.toString(height);
+		bw.append(result);
+		bw.flush();
+
+		bw.close();
+		br.close();
+
+	}
+
+}
