fe07c59e37400f353160f355d70502c81682039b
최정필 <wjdvlf1407@gmail.com>
2024-08-03 09:40:27 +0900

    1111

---
+++ src/com/baekjoon/study/Ex2965_캥거루세마리.java
@@ -0,0 +1,30 @@
+package com.baekjoon.study;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Ex2965_캥거루세마리 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+
+		if (c - b >= b - a && c - b > 1) {
+			System.out.println(c - b - 1);
+		} else if (c - b < b - a && b - a > 1) {
+			System.out.println(b - a - 1);
+
+		} else {
+			System.out.println(0);
+		}
+
+		br.close();
+	}
+
+}
