47fd3edc4e9f072a60ebaf23a246a91e50d1d118
최정필 <wjdvlf1407@gmail.com>
2024-08-08 09:08:01 +0900

    11

--- src/com/baekjoon/study/Ex10817_세수.java
+++ src/com/baekjoon/study/Ex10817_세수.java
@@ -16,18 +16,24 @@ public class Ex10817_세수 {
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		
 
-		int[] abc = new int[3];
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
 
-		for (int i = 0; i < abc.length; i++) {
-			abc[i] = Integer.parseInt(st.nextToken());
+		int result = 0;
+
+		if ((a >= b && b >= c) || (a <= b && b <= c)) {
+			result = b;
+		} else if ((b >= c && c >= a) || (b <= c && c <= a)) {
+			result = c;
+		} else if ((c >= a && a >= b) || (c <= a && a <= b)) {
+			result = a;
+		} else {
+
 		}
-		
-		
 
-
+		bw.append(Integer.toString(result));
 
 		bw.flush();
 		bw.close();
