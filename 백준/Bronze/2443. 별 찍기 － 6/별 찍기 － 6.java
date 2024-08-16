4eba2ff57efef3629a68a2bef9fd8be603bfb333
최정필 <103472023+wjdvlf5456@users.noreply.github.com>
2024-08-16 17:17:34 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 76 ms, Memory: 11864 KB -BaekjoonHub

---
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int n = Integer.parseInt(br.readLine());
+
+		for (int i = n; i > 0; i--) {
+			for (int j = n - i; j > 0; j--) {
+				bw.append(" ");
+			}
+			for (int j = 1; j < 2 * i; j++) {
+				bw.append("*");
+			}
+			bw.newLine();
+		}
+
+		bw.flush();
+
+		bw.close();
+		br.close();
+
+	}
+}
