04226dd8d03a0a4f66e15cb2012ad69d89343ded
최정필 <wjdvlf1407@gmail.com>
2024-08-30 13:44:44 +0900

    팰린드롬 수

---
+++ src/com/baekjoon/study/Ex1259_팰린드롬수.java
@@ -0,0 +1,46 @@
+package com.baekjoon.study;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Ex1259_팰린드롬수 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		String n = "";
+
+		StringBuilder sb = new StringBuilder();
+
+		while (true) {
+			n = br.readLine();
+
+			if (n.charAt(0) == '0') {
+				break;
+			} else {
+				sb.append(n);
+				sb.reverse();
+
+				if (sb.toString().equals(n)) {
+					bw.append("yes");
+				} else {
+					bw.append("no");
+				}
+				sb.delete(0, sb.length());
+
+				bw.flush();
+				bw.newLine();
+
+			}
+
+		}
+
+		bw.close();
+		br.close();
+
+	}
+
+}
