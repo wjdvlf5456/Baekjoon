8149968c7f9dc0d495faa61fc2f08239d15558c4
최정필 <wjdvlf1407@gmail.com>
2024-09-05 09:04:05 +0900

    11

---
+++ src/com/baekjoon/study/Ex2587_대푯값2.java
@@ -0,0 +1,35 @@
+package com.baekjoon.study;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Ex2587_대푯값2 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int[] nArr = new int[5];
+		int sum = 0;
+
+		for (int i = 0; i < 5; i++) {
+			nArr[i] = Integer.parseInt(br.readLine());
+			sum += nArr[i];
+		}
+
+		Arrays.sort(nArr);
+
+		bw.append(Integer.toString(sum / 5));
+		bw.newLine();
+		bw.append(Integer.toString(nArr[2]));
+
+		bw.close();
+		br.close();
+
+	}
+
+}
