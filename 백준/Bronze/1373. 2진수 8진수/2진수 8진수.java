70dfc639ff230a275c2283981123327e00560ac4
최정필 <103472023+wjdvlf5456@users.noreply.github.com>
2024-09-17 12:04:53 +0900

    [Bronze I] Title: 2진수 8진수, Time: 1796 ms, Memory: 94972 KB -BaekjoonHub

---
+++ 백준/Bronze/1373. 2진수 8진수/2진수 8진수.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.math.BigInteger;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		BigInteger N = new BigInteger(br.readLine(), 2);
+		String oct = N.toString(8);
+
+		bw.append(oct);
+
+		bw.flush();
+
+		bw.close();
+		br.close();
+
+	}
+
+}
